661cd56df4bd487d004768e0e2b2334569f98b61
Hezrongg <hezrongg26@gmail.com>
2024-10-11 12:41:16 +0800

    initial commit

---
+++ numberGuess.js
@@ -0,0 +1,14 @@
+function guessTheNumber() {
+    let number = 3;
+    let enter = prompt("Guess a number between 1 and 10:");
+    while (enter != number) {
+        if (enter < number) {
+            alert("Too low")
+        } else {
+            alert("Too High")
+        }
+        enter = prompt("Guess a number between 1 and 10:");
+    }
+    alert("Correct");
+}
+
